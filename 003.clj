89c108e9347d58cdf1b047b5283aca3c720ce532
Konstantin Shabanov <etehtsea@gmail.com>
2013-08-07 15:36:41 +0700

    Problem #3

---
+++ 003.clj
@@ -0,0 +1,14 @@
+(ns projecteuler.problem3
+  (:require [clojure.test :refer [with-test is]]))
+
+(defn prime? [n]
+  (.isProbablePrime (BigInteger/valueOf n) 5))
+
+(with-test
+  (defn largest-prime-factor [number]
+    (let [upper (Math/round (Math/sqrt number))
+          primes (filter prime? (range upper 2 -1))]
+      (some #(if (zero? (mod number %)) %) primes)))
+  (is (= 29 (largest-prime-factor 13195))))
+
+(clojure.test/run-tests 'projecteuler.problem3)

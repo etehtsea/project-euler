cdc1b99ad3e6a225842745291993fbd7cea55ae2
Konstantin Shabanov <etehtsea@gmail.com>
2013-08-18 00:17:16 +0700

    Problem #23 (improved)

--- 023.clj
+++ 023.clj
@@ -7,19 +7,14 @@
 (defn proper-divisors-sum [n]
   (reduce + (proper-divisors n)))
 
-(defn abundant? [n]
-  (> (proper-divisors-sum n) n))
-
 (def max-num (inc 20161))
 
-(def abundants
-  (vec (filter abundant? (range 12 max-num))))
-
-(defn abundants-sums []
-  (for [x abundants y abundants
-        :while (<= y x)]
-    (+ x y)))
+(def abundants-sums
+  (let [abundant? #(> (proper-divisors-sum %) %)
+        abundants (vec (filter abundant? (range 12 max-num)))]
+    (set (for [x abundants y abundants
+               :while (<= y x)]
+           (+ x y)))))
 
 (defn result []
-  (reduce + (clojure.set/difference (set (range 1 max-num))
-                                    (set (abundants-sums)))))
+  (reduce + (remove abundants-sums (range 1 max-num))))
